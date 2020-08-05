fd1277d84582002c00569577f5be25f07c054046
Calvin Sung <calvinsungvin@gmail.com>
2020-08-05 13:04:52 +0800

    feat: add in 'no repetitive url' feature

--- app.js
+++ app.js
@@ -22,21 +22,31 @@ app.get('/', async (req, res) => {
     res.render('index', {shortUrls})
 })
 
+
 app.post('/shortUrls', async (req, res) => {
-    await ShortUrl.create({
-        full: req.body.fullUrl,
-        short: makeId(5)
-    })  //req.body -> 從form裡面拿資料進入mongodb database. { full: xxx } -> 針對 shortUrls.js 此file. req.body.fullUrl ->針對index.handlebars內的name
+    const { fullUrl } = req.body
+    if (fullUrl){
+        const exists = ShortUrl.some(
+            url => ShortUrl.full === fullUrl
+        )
+        if (!exists){
+            await ShortUrl.create({
+                full: req.body.fullUrl,
+                short: makeId(5)
+            })  //req.body -> 從form裡面拿資料進入mongodb database. { full: xxx } -> 針對 shortUrls.js 此file. req.body.fullUrl ->針對index.handlebars內的name
+        }
+    } 
     res.redirect('/')
 })
+
+
+
 
 app.get('/:shortUrl', async (req, res) => {
     const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
     if (shortUrl === null) return res.sendStatus(404)
-
     res.redirect(shortUrl.full)
 })
-
 
 // start and listen on the Express server
 app.listen(port, () => {
